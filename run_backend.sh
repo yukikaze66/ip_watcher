45b9ec5e220282593f6e8b8477cd3ccec52626da
thiswind <thiswind@gmail.com>
2018-04-17 14:23:29 +0800

    update ping test

--- run_backend.sh
+++ run_backend.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/bash
 
 git pull
-sudo pip install -r requirements.txt
+sudo pip install -r gear/requirements.txt
 sudo python gear/export.py
 sudo chmod 777 _data/*
+git add .
 git commit -m 'update ping test'
 git push
