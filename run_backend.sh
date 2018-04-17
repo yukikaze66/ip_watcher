c8a5ac80b8be9258a3f824821a0a14679df7d951
thiswind <thiswind@gmail.com>
2018-04-17 07:50:16 +0000

    site update

--- run_backend.sh
+++ run_backend.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 git pull
 sudo -H pip install -r gear/requirements.txt
@@ -6,5 +6,7 @@ sudo -H python gear/export.py
 sudo -H chmod 777 _data/*
 git add .
 git commit -m 'update ping test'
+git config --global user.name "thiswind"
+git config --global user.email thiswind@gmail.com
 git config --global push.default simple
 git push
