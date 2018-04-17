d4a5964bf1a3f1a2afbda21922bfefb9c4728014
root <root@thiswind-dev.ynuosa.org>
2018-04-17 15:46:15 +0800

    update ping test

--- run_backend.sh
+++ run_backend.sh
@@ -6,5 +6,7 @@ sudo -H python gear/export.py
 sudo -H chmod 777 _data/*
 git add .
 git commit -m 'update ping test'
+git config --global user.name "thiswind"
+git config --global user.email thiswind@gmail.com
 git config --global push.default simple
 git push
