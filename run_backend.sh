71578499bcc40110bda1c48f2e9690ab3b3a4036
thiswind <thiswind@gmail.com>
2018-04-17 17:54:29 +0800

    remove sudo for docker

--- run_backend.sh
+++ run_backend.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 git pull
-sudo -H pip install -r gear/requirements.txt
-sudo -H python gear/export.py
-sudo -H chmod 777 _data/*
+pip install -r gear/requirements.txt
+python gear/export.py
+chmod 777 _data/*
 git add .
 git commit -m 'update ping test'
 git config --global user.name "thiswind"
